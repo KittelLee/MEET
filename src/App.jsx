509cf7f760550ec7a79050b563b68e947a1cc529
Leejinuk <leejin7900@gmail.com>
2025-12-22 23:06:40 +0900

    Feat: Header Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import bars from "../assets/icons/bars.svg";
+import "../styles/Header.css";
+
 function Header() {
-  return <div>Header</div>;
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsNavOpen(false);
+  };
+  return (
+    <header className="header">
+      <div className="header-wrap">
+        <div className="logo-box">
+          <Link to="/" className="logo">
+            <p>🐣수도권2030🐣 서울경기 💖우리들의 놀이💖</p>
+          </Link>
+        </div>
+        <nav className={`navbar ${isNavOpen ? "active" : ""}`}>
+          <ul className="nav">
+            <li>
+              <Link to="/rule" onClick={handleLinkClick}>
+                모임회칙
+              </Link>
+            </li>
+            <li>
+              <Link to="/room" onClick={handleLinkClick}>
+                방이동
+              </Link>
+            </li>
+            <li>
+              <Link to="/calc" onClick={handleLinkClick}>
+                정산하기
+              </Link>
+            </li>
+            <li>
+              <Link to="/out" onClick={handleLinkClick}>
+                외출신청
+              </Link>
+            </li>
+            <li>
+              <Link to="/suggestion" onClick={handleLinkClick}>
+                익명 건의함
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <button className="toggle-btn" onClick={toggleNav}>
+          <img src={bars} alt="메뉴바" />
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
