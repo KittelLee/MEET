d30eced49c6c049a19aca78c400c1287f24d1ee6
Leejinuk <leejin7900@gmail.com>
2025-12-23 11:27:18 +0900

    Feat: Desktop IMG Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Rule from "./pages/Rule";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 500);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>
-      <Header />
+    <>
+      {isMobile ? (
+        <div>
+          <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rule" element={<Rule />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rule" element={<Rule />} />
+          </Routes>
 
-      <Footer />
-    </div>
+          <Footer />
+        </div>
+      ) : (
+        <div className="desktop-message">
+          📢 모바일 환경에서만 볼 수 있습니다.
+        </div>
+      )}
+    </>
   );
 }
 
